e12a2e98b0b497bf078fe18f2e4b1709a5bf0825
zach-babb-moovel <zach.babb@moovel.com>
2017-02-22 15:33:17 -0800

    Testing automatic deployment from source changes

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ exports.handler = function(event, context, callback) {
   var response = {
     statusCode: 200,
     headers: {},
-    body: 'HELLO WORLD! Maybe...'
+    body: {
+      foo: 'I am an object now.'
+    }
   };
   callback(null, response);
 };
