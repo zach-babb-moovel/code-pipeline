34e5f13a9a2e2bcdca1ce8b3c669eea1ec912426
zach-babb-moovel <zach.babb@moovel.com>
2017-02-22 16:57:03 -0800

    Testing node module

--- next.js
+++ next.js
@@ -1,8 +1,15 @@
+var _ = require('lodash');
+
 exports.handler = function(event, context, callback) {
+
+  var test = event.pathParameters.test;
+  var stuff = [1, 2, 3];
+  stuff = _.map(stuff, function(item) { return item + 5; });
+
   var response = {
     statusCode: 200,
     headers: {},
-    body: JSON.stringify(event)
+    body: JSON.stringify({test: test, stuff: stuff})
   };
   callback(null, response);
 };
