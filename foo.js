76f87c3bd67a3e860846743d92d197690ec4f3f3
zach-babb-moovel <zach.babb@moovel.com>
2017-02-23 13:41:45 -0800

    Foo fighter

---
+++ foo.js
@@ -0,0 +1,15 @@
+var _ = require('lodash');
+
+exports.handler = function(event, context, callback) {
+
+  var test = event.pathParameters.bar;
+  var stuff = [1, 2, 3];
+  stuff = _.map(stuff, function(item) { return item + 5; });
+
+  var response = {
+    statusCode: 200,
+    headers: {},
+    body: JSON.stringify({test: test, stuff: stuff})
+  };
+  callback(null, response);
+};
