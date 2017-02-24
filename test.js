a95371bd7dd7bbe12dc5b0fbc6423573c8d5b692
zach-babb-moovel <zach.babb@moovel.com>
2017-02-24 10:42:48 -0800

    Adding a test to test testing tests

---
+++ test.js
@@ -0,0 +1,11 @@
+var mocha = require('mocha');
+var chai = require('chai');
+var assert = chai.assert;
+
+describe('test', function() {
+  it('should pass this test', function() {
+    assert(false, 'There is a problem with the test');
+  });
+});
+
+
